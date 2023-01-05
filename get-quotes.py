37dda699c094f0b0b366d391f71aeac4f9d715db
Rick Dawson <dawsora@gmail.com>
2023-01-05 15:21:14 -0500

    added requirements.txt

--- get-quotes.py
+++ get-quotes.py
@@ -23,10 +23,13 @@ if __name__ == "__main__":
     while miss_count < 10 and count < 200:
         print(f"Total {db.estimated_document_count()}:\n\tNew {count}\tMiss: {miss_count}")
         quote = requests.get("https://api.quotable.io/random")
-        check_id = quote.json()["_id"]
-        try:
-            db.insert_one(quote.json())
-            count += 1
-        except(pymongo.errors.DuplicateKeyError):
-            miss_count += 1
+        if quote.json()["_id"]:
+            check_id = quote.json()["_id"]
+            try:
+                db.insert_one(quote.json())
+                count += 1
+            except(pymongo.errors.DuplicateKeyError):
+                miss_count += 1
+        else:
+            print(f"No id found for {quote.json()}")
 
